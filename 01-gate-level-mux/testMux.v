eea6429b34946bee886d04bb5899053da3caf4ef
Emre Ezgü <emrezgu@gmail.com>
2026-08-15 13:51:53 +0300

    Update testMux.v

--- 01-gate-level-mux/testMux.v
+++ 01-gate-level-mux/testMux.v
@@ -1,13 +1,13 @@
 `timescale 1ns/1ns
-module testMuxG ();
+module testMux ();
     reg input0;
     reg input1;
     reg select;
     wire o;
 
-    muxG variable_name(input0,input1,select,o);
+    mux variable_name(input0,input1,select,o);
     initial begin
-        $dumpfile("muxGtime.vcd");
+        $dumpfile("muxtime.vcd");
         $dumpvars(0,input0,input1,select,o);
         input0=1'b1;input1=1'b0;
         select=1'b0;
